c3ca51f79256b3e5c8b2e76b5ae5d397ba678561
crisrocha025 <63604580+crisrocha025@users.noreply.github.com>
2020-05-26 12:06:49 -0300

    Nem sei como explicar isso

--- seletorlabel.js
+++ seletorlabel.js
@@ -1,10 +1,10 @@
- Items = []
+   Items = []
     for(item=0;item<20;item++){
       var go = ':nth-child('+item+')'
       Items.push(go)
       }
       Labell=[]
      for(l=0;l<20;l++){
-        const labbel = document.querySelector('.labelName'+l)
+        const labbel = document.querySelector(`.label-head${Items[l]}`)
         Labell.push(labbel)
      }
