1145775732dc1d754ad9ae42334cffafaa5bd83a
crisrocha025 <63604580+crisrocha025@users.noreply.github.com>
2020-05-26 13:17:18 -0300

    Update seletorlabel.js

--- seletorlabel.js
+++ seletorlabel.js
@@ -1,4 +1,5 @@
-   Items = []
+ function Incicio(){
+      Items = []
     for(item=0;item<20;item++){
       var go = ':nth-child('+item+')'
       Items.push(go)
@@ -8,3 +9,28 @@
         const labbel = document.querySelector(`.labelName${Items[l]}`)
         Labell.push(labbel)
      }
+    for(m=0;m<20;m++){
+      if(Labell[m] != null){
+        var elLabel = Labell[m]
+        labelSelect(elLabel)
+      }
+    }
+    
+ }
+
+function labelSelect(elemento){
+    const labelArray = elemento.innerHTML.split('_')
+    labelArray.forEach((cont,i) => {
+      if(i==1){
+        passaValor(cont)
+      }
+    })
+ function passaValor(ValorP){
+   var valorProduto = document.querySelector('.valorp')
+   valorProduto.innerHTML = ValorP
+ }
+  
+}
+
+
+      window.load = Incicio()
