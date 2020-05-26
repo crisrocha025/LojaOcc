7c09709376361bd8460caeb756c64f25007386d4
crisrocha025 <63604580+crisrocha025@users.noreply.github.com>
2020-05-26 11:47:18 -0300

    Create seletorlabel.js

---
+++ seletorlabel.js
@@ -0,0 +1,10 @@
+   Items = []
+    for(item=0;item<20;item++){
+      var go = ':nth-child('+item+')'
+      Items.push(go)
+      }
+      Labell=[]
+     for(l=0;l<20;l++){
+        const labbel = document.querySelector('label-head'+l)
+        Labell.push(labbel)
+     }
