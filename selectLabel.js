b8de1ac71ea01ed28e92762b1c323047a5013e94
crisrocha025 <63604580+crisrocha025@users.noreply.github.com>
2020-05-28 12:19:42 -0300

    Create selectLabel.js

---
+++ selectLabel.js
@@ -0,0 +1,11 @@
+       var labelName = document.querySelectorAll(`.labelName`)
+        for (var j = 0; j < labelName.length; j++){
+           var labelSelect = labelName[j].innerHTML.split('_')
+           if(labelSelect[1]){
+            labelName[j].innerHTML = labelSelect[1]
+           }else{
+            labelName[j].remove()
+           }
+           console.log(labelSelect[1])
+        }
+     
