f2d0b9208ec1c7e982a8075532d1ce4a87e8d4cd
crisrocha025 <63604580+crisrocha025@users.noreply.github.com>
2020-05-29 10:40:59 -0300

    Update selectLabel.js

--- selectLabel.js
+++ selectLabel.js
@@ -2,8 +2,7 @@
         for (var j = 0; j < labelName.length; j++){
            var labelSelect = labelName[j].innerHTML.split('_')
            if(labelSelect[1]){
-             var valorFinal = labelSelect[1].split('.')
-             labelName[j].innerHTML = 'R$ ' + valorFinal[0] +','+ valorFinal[1]
+             labelName[j].innerHTML = 'R$ ' +labelSelect[1]
             
            }else{
             labelName[j].remove()
