5b44ebb75d94facbf37239a7f6b4eaf5f5e2cb82
crisrocha025 <63604580+crisrocha025@users.noreply.github.com>
2020-06-01 17:25:57 -0300

    Update produtoDigital.js

--- produtoDigital.js
+++ produtoDigital.js
@@ -1,9 +1,11 @@
   var labelName = document.querySelectorAll(`.labelName`)
   var valorProdutoD = document.querySelector(`.valor`)
+  var valorInicio = document.querySelector(`.valorInicio`)
         for (var j = 0; j < labelName.length; j++){
            var labelSelect = labelName[j].innerHTML.split('_')
            if(labelSelect[1]){
              valorProdutoD.innerHTML = labelSelect[1]
+             valorInicio.innerHTML = labelSelect[1]
            }
            console.log(labelSelect[1])
         }
